b5c39290a7bb9ab01f0e0f320b85e4ca86167959
Christian Stprix <christprix@gmail.com>
2023-11-19 21:44:09 -0500

    added button styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
 import { useState } from 'react'
 import './App.css'
 import student from '/student.gif'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserTie, faChildren, faDragon, faUserNinja } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
 
+  function handleClick(e) {
+    e.preventDefault
+    console.log(e.currentTarget);
+    document.querySelectorAll(".option").forEach((div) => {
+      div.classList.remove('selected')
+    })
+    e.currentTarget.classList.add("selected")
+  }
   return (
     <>
       <div className='wrapper'>
-        <h1>Kanji Learner</h1>
+        <h1 className='title'>Kanji Learner</h1>
         <img className='mainImage' src={student} alt="" />
         <p className='message'>Select a Difficulty</p>
         <div className='options'>
-          <button className='option'>1</button>
-          <button className='option'>2</button>
-          <button className='option'>3</button>
-          <button className='option'>4</button>
+          <button className='option' onClick={handleClick}><FontAwesomeIcon icon={faChildren} size='xl' /></button>
+          <button className='option' onClick={handleClick}><FontAwesomeIcon icon={faUserTie} size='xl' /></button>
+          <button className='option' onClick={handleClick}><FontAwesomeIcon icon={faUserNinja} size='xl' /></button>
+          <button className='option' onClick={handleClick}><FontAwesomeIcon icon={faDragon} size='xl' /></button>
         </div>
         <button className='btn'>Start Game</button>
       </div>
