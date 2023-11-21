421fb4a00fbed849899adf1de125f5c75b3c1006
Christian Stprix <christprix@gmail.com>
2023-11-21 00:51:10 -0500

    created button styling on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,50 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import Button from './Button'
+import axios from 'axios'
 import student from '/student.gif'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserTie, faChildren, faDragon, faUserNinja } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  const [gameStart, setGameStart] = useState(false)
+  const [kanji, setKanji] = useState('wait')
+  const [message, setMessage] = useState('')
+  const [icons, setIcons] = useState(ICONS)
+
+  function start(e) {
+    axios.get(`https://kanjiapi.dev/v1/kanji/kyoiku`)
+      .then(res => {
+        const multiplier = res.data.length
+        let num = Math.floor(Math.random() * multiplier)
+        function getnum() {
+          return Math.floor(Math.random() * multiplier)
+        }
+        axios.get(`https://kanjiapi.dev/v1/kanji/${res.data[`${num}`]}`)
+          .then(response => {
+            let kanji_data = response.data
+            setKanji([{
+              id: kanji_data.unicode,
+              character: kanji_data.kanji,
+              meaning: decodeString(kanji_data.meanings['0']),
+              options: [res.data[getnum()], res.data[getnum()], res.data[getnum()], kanji_data.kanji].sort(() => Math.random() - 0.5)
+            }
+            ]);
+            console.log(kanji);
+            console.log(icons);
+            setGameStart(true)
+          })
+      })
+  }
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
 
   function handleClick(e) {
+    console.log(kanji);
     e.preventDefault
     document.querySelectorAll(".option").forEach((div) => {
       div.classList.remove('selected')
@@ -24,16 +62,19 @@ function App() {
           {/* <img className='mainImage' src={student} alt="" /> */}
           <p className='message'>Select your Destiny &quot;Difficuly&quot;</p>
           <div className='options'>
-            <button className='option' onClick={handleClick}><FontAwesomeIcon icon={faChildren} size='xl' /></button>
-            <button className='option' onClick={handleClick}><FontAwesomeIcon icon={faUserTie} size='xl' /></button>
-            <button className='option' onClick={handleClick}><FontAwesomeIcon icon={faUserNinja} size='xl' /></button>
-            <button className='option' onClick={handleClick}><FontAwesomeIcon icon={faDragon} size='xl' /></button>
+            <Button kanji={kanji} key={1} icon={icons} gameStart={gameStart} />
           </div>
-          <button className='btn'>Start Game</button>
+          <button className='btn' onClick={start}>Start Game</button>
         </div>
       </div>
     </>
   )
 }
 
+const ICONS = [
+  { icon: <FontAwesomeIcon icon={faChildren} size='xl' /> },
+  { icon: <FontAwesomeIcon icon={faUserTie} size='xl' /> },
+  { icon: <FontAwesomeIcon icon={faUserNinja} size='xl' /> },
+  { icon: <FontAwesomeIcon icon={faDragon} size='xl' /> }
+]
 export default App
---
+++ src/Button.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react'
+
+export default function Button({ icon, kanji }) {
+  const [display, setDisplay] = useState(icon)
+  const [answer, setAnswer] = useState('test')
+  useEffect(() => {
+    console.log(display);
+    console.log(kanji[0].character);
+    setAnswer(kanji[0].character)
+    document.querySelectorAll(".option").forEach((div) => {
+      div.classList.remove('selected');
+      div.classList.remove('wrong');
+      div.classList.remove('correct');
+    })
+  }, [kanji])
+
+  function handleClick(e) {
+    e.preventDefault
+    document.querySelectorAll(".option").forEach((div) => {
+      div.classList.remove('selected')
+    })
+    e.currentTarget.classList.add("selected")
+  }
+
+  function handleClick2(e) {
+    e.preventDefault
+    console.log(e.currentTarget.innerHTML);
+    if (e.currentTarget.innerHTML === answer) {
+      e.currentTarget.disabled = true;
+      e.currentTarget.classList.add("correct")
+    }
+    else {
+      e.currentTarget.disabled = true;
+      e.currentTarget.classList.add("wrong")
+    }
+  }
+  if (kanji === 'wait') {
+    return (
+      <>
+
+        {
+          icon.map((i, index) => {
+            {
+              return <button className="option" key={index} onClick={handleClick}>{i.icon}</button>
+            }
+          })
+        }
+      </>
+    )
+  }
+  else {
+    return (
+      <>
+        {
+          kanji[0].options.map((k, index) => {
+            return <button className="option" key={index} onClick={handleClick2}>{k}</button>
+          })
+        }
+      </>
+    )
+  }
+}
